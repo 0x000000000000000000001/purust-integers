52441f446ab2ec1ff221210e85b114fc9b3a5e7d
0x1 <kevin.francart3@gmail.com>
2026-08-11 12:53:05 +0200

    misc

---
+++ src/Data/Int.rs
@@ -0,0 +1,5 @@
+
+pub fn Data_Int_fromStringAsImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Int_fromNumberImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Int_toNumber() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Int_toStringAs() -> crate::UnknownType { crate::UnknownType::new(0) }
