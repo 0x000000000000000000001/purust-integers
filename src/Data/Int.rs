54b3479c08bcef9f6c40f774575deb6e652229e2
0x1 <kevin.francart3@gmail.com>
2026-08-12 09:18:51 +0200

    misc

--- src/Data/Int.rs
+++ src/Data/Int.rs
@@ -1,5 +1,5 @@
 
-pub fn Data_Int_fromStringAsImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Data_Int_fromNumberImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Data_Int_toNumber() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Data_Int_toStringAs() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Int_fromStringAsImpl() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Data_Int_fromNumberImpl() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Data_Int_toNumber() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Data_Int_toStringAs() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
