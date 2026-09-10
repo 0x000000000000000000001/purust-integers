12e262b3adc3c220d434682d2bb4e719ddb092cb
0x1 <kevin.francart3@gmail.com>
2026-09-10 21:44:06 +0200

    misc

--- src/Data/Int.rs
+++ src/Data/Int.rs
@@ -1,6 +1,4 @@
-pub fn Data_Int_toStringAs(mut radix_val: crate::UnknownType, mut n_val: crate::UnknownType) -> crate::UnknownType {
-    let radix = radix_val.unwrap_int() as u32;
-    let n = n_val.unwrap_int();
+pub fn Data_Int_toStringAs(radix: i64, n: i64) -> String {
     let s = if radix == 16 {
         format!("{:x}", n)
     } else if radix == 10 {
@@ -12,43 +10,34 @@ pub fn Data_Int_toStringAs(mut radix_val: crate::UnknownType, mut n_val: crate::
     } else {
         panic!("Unsupported radix: {}", radix);
     };
-    crate::Value::String(s)
+    s
 }
 
-pub fn Data_Int_toNumber(mut n_val: crate::UnknownType) -> crate::UnknownType {
-    let n = n_val.unwrap_int();
-    crate::Value::Number(n as f64)
+pub fn Data_Int_toNumber(n: i64) -> f64 {
+    n as f64
 }
 
-pub fn Data_Int_quot(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
-    let a = a0.unwrap_int();
-    let b = a1.unwrap_int();
-    crate::mk_int(a / b)
+pub fn Data_Int_quot(a: i64, b: i64) -> i64 {
+    a / b
 }
 
-pub fn Data_Int_rem(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
-    let a = a0.unwrap_int();
-    let b = a1.unwrap_int();
-    crate::mk_int(a % b)
+pub fn Data_Int_rem(a: i64, b: i64) -> i64 {
+    a % b
 }
 
-pub fn Data_Int_pow(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
-    let a = a0.unwrap_int();
-    let b = a1.unwrap_int();
-    crate::mk_int(a.pow(b as u32))
+pub fn Data_Int_pow(a: i64, b: i64) -> i64 {
+    a.pow(b as u32)
 }
 
 pub fn Data_Int_fromNumberImpl(
-    mut just: crate::UnknownType,
-    mut nothing: crate::UnknownType,
-    mut n: crate::UnknownType,
-) -> crate::UnknownType {
-    let num = n.unwrap_number();
+    just: purust_core::Func1<UnknownType, std::rc::Rc<Purs_Data_Maybe::Maybe>>,
+    nothing: std::rc::Rc<Purs_Data_Maybe::Maybe>,
+    num: f64,
+) -> std::rc::Rc<Purs_Data_Maybe::Maybe> {
     if num.is_finite() && num.fract() == 0.0 && num >= (-2147483648.0) && num <= (2147483647.0) {
         let int_val = num as i64;
-        just.unwrap_func1()(crate::mk_int(int_val))
+        just(crate::mk_int(int_val))
     } else {
-        eprintln!("fromNumberImpl returning Nothing for num={}", num);
         nothing.clone()
     }
 }
